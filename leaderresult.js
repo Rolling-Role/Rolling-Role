373f53168c8ee33ae768e2d793ecd7cfaeb26ebc
Dawn Wi <wi825@naver.com>
2020-11-15 21:38:13 +0900

    리더결과 js 추가

---
+++ leaderresult.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#leader_tab').click(function(){
+        window.location.href="leader.html"
+    });
+    $('#role_tab').click(function(){
+        window.location.href="role_setting/role_setting.html"
+    });
+    $('#vote').click(function(){
+        window.location.href="vote2.html"
+    });
+});
