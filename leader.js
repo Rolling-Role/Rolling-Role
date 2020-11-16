71d56d08ae5cdaeb7a412a053f6342b9920bdc3f
GyeongTak Byeon <bkt1214bkt@naver.com>
2020-11-16 11:21:29 +0900

    네번째 커밋. js 추가

---
+++ leader.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#role_tab').click(function(){
+        window.location.href="role_setting/role_setting.html"
+    });
+});
+
+$(document).ready(function(){
+    $('#start_hover').click(function(){
+        window.location.href="dice/dice.html"
+    });
+});
