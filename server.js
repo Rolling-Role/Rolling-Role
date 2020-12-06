6e188bd7c0a04b4a83b17d0712bde5097b65a357
tlfzgjadyd <tlfzgjadyd@naver.com>
2020-12-06 19:57:21 +0900

    server.js

--- server.js
+++ server.js
@@ -1,135 +1,195 @@
-// // 필요한 것들을 넣어줄것
-// var http = require('http');
-// var fs = require('fs');
-// var qs = require('querystring');
-// var mysql = require('mysql');
-// var app = http.createServer(function(request,response){
-//     var url = request.url; //내가 처리하고싶은 url
+// 필요한 것들을 넣어줄것
+var http = require('http');
+var fs = require('fs');
+var qs = require('querystring');
+var mysql = require('mysql');
+var app = http.createServer(function(request,response){
+    var url = request.url; //내가 처리하고싶은 url
 
 
-//     if (request.method == 'GET') { //get으로 전달된거면 이부분 실행
-//         if(request.url == '/'){
+    if (request.method == 'GET') { //get으로 전달된거면 이부분 실행
+        if(request.url == '/'){
 
-//             url = '/index.html';  //기본적으로 index.html이 실행됨
-//         }else if(request.url == '/favicon.ico'){
-//             response.writeHead(404); //바로가기 아이콘이 없으면 404 리턴
-//             response.end();
-//             return;
-//         }else {
-//             url = request.url; //기본이 아니라면 해당 url을 저장
-//         }
+            url = '/html/index.html';  //기본적으로 index.html이 실행됨
+        }else if(request.url == '/favicon.ico'){
+            response.writeHead(404); //바로가기 아이콘이 없으면 404 리턴
+            response.end();
+            return;
+        }else {
+            url = request.url; //기본이 아니라면 해당 url을 저장
+        }
 
-//         response.writeHead(200); //잘 열렸다고 200으로 확인
-//         response.end(fs.readFileSync(__dirname + url)); //__dirname = 현재 서버가 있는 디렉터리
-//         return;
-//     }
+        response.writeHead(200); //잘 열렸다고 200으로 확인
+        response.end(fs.readFileSync(__dirname + url)); //__dirname = 현재 서버가 있는 디렉터리
+        return;
+    }
 
-//     if (request.method == 'POST') { 
-//         var body = '';
+    if (request.method == 'POST') { 
+        var body = '';
 
-//         request.on('data', function (data) { //post로 전달된 데이터가 있으면 body에 차곡차곡 쌓아라
-//             body += data;
+        request.on('data', function (data) { //post로 전달된 데이터가 있으면 body에 차곡차곡 쌓아라
+            body += data;
 
-//             if (body.length > 1e6)
-//                 request.connection.destroy();
-//         });
+            if (body.length > 1e6)
+                request.connection.destroy();
+        });
 
-//         request.on('end', function () { //body로 들어온 데이터를 가공하여 뚝딱뚝딱해서 객체화시킴
-//             var post = qs.parse(body); //ex) 투표에서 찬성이 들어왔으면 객체화하여 출력
-//             console.dir(post);
+        request.on('end', function () { //body로 들어온 데이터를 가공하여 뚝딱뚝딱해서 객체화시킴
+            var post = qs.parse(body); //ex) 투표에서 찬성이 들어왔으면 객체화하여 출력
+            console.dir(post);
 
-//             var text = request.url.split('/');
-//             var method = text[1];
+            var text = request.url.split('/');
+            var method = text[1];
         
-//             if(typeof(postMethods[method]) == 'undefined'){ //request.url이 정의되어 있지 않으면
-//                 response.writeHead(404); //404에러 출력
-//                 response.end();
-//                 return;
-//             }else{
-//                 console.dir(postMethods); //request.url이 존재하면 postMethods 실행하라
-//                 postMethods[method](response,post);//post값 전달하는 과정
-//             }
+            if(typeof(postMethods[method]) == 'undefined'){ //request.url이 정의되어 있지 않으면
+                response.writeHead(404); //404에러 출력
+                response.end();
+                return;
+            }else{
+                console.dir(postMethods); //request.url이 존재하면 postMethods 실행하라
+                postMethods[method](response,post);//post값 전달하는 과정
+            }
 
 
 
-//             // use post['blah'], etc.
-//         });
-//     }
+            // use post['blah'], etc.
+        });
+    }
 
-// });
-// app.listen(3000); //localhost:3000으로 서버에 접속. 자신은 3000을 들을 준비가 되어있음
-
-
-// var connInfo = {
-//     host     : 'localhost',    // 호스트 주소
-//     user     : 'root',           // mysql user
-//     password : 'tak1214',       // mysql password
-//     database : 'example'         // mysql 데이터베이스
-// };
-
-// var queryExecute = function(sql,callback){
-
-//     var connection = mysql.createConnection(connInfo);
-//     connection.connect();
-//     connection.query(sql,callback);
-//     connection.end();
-// };
-
-// var send200 = function(response,str){
-
-//     response.writeHead(200);
-//     response.write(str);
-//     response.end();
-// };
+});
+app.listen(80); //localhost:80으로 서버에 접속. 자신은 3000을 들을 준비가 되어있음
 
 
-// var getData = function(param,callback){
+var connInfo = {
+    host     : 'localhost',   
+    port     : '3307',
+    user     : 'roll',       
+    password : '1234',     
+    database : 'rollingrole'    
+};
 
-//     var sql = 'select * from example';
-//     queryExecute(sql,function (error, results, fields) {
-//         if (error) throw error;
+var queryExecute = function(sql,callback){
 
-//         callback(results);
-//     });
+    var connection = mysql.createConnection(connInfo);
+    connection.connect();
+    connection.query(sql,callback);
+    connection.end();
+};
 
-// };
+var send200 = function(response,str){
 
-// var postMethods = {};
+    response.writeHead(200);
+    response.write(str);
+    response.end();
+};
 
 
-// //---------
-// postMethods.saveData = function(res,post){
+
+var getData = function(param,callback){
+
+    var sql = 'select * from example';
+    queryExecute(sql,function (error, results, fields) {
+        if (error) throw error;
+
+        callback(results);
+    });
+
+};
+
+var postMethods = {};
+
+
+//---------
+postMethods.saveData = function(res,post){
     
-//     var sql = 'insert into tttt (name) values ("'+post.name+'")';
-//     queryExecute(sql,function (error, results, fields) {
-//         if (error) throw error;
-//         var returnStr = '';
-//         send200(res,returnStr)
-//     });
+    var sql = 'insert into tttt (name) values ("'+post.name+'")';
+    queryExecute(sql,function (error, results, fields) {
+        if (error) throw error;
+        var returnStr = '';
+        send200(res,returnStr)
+    });
 
-// };
+};
 
-// postMethods.saveVote = function(res,post){
+postMethods.saveVote = function(res,post){
 
-//     console.dir(1123);
+    console.dir(1123);
     
-//     var sql = 'insert into info (name) values ("'+post.vote+'")';
-//     queryExecute(sql,function (error, results, fields) {
-//         if (error) throw error;
-//         var returnStr = '';
-//         send200(res,returnStr)
-//     });
+    var sql = 'insert into info (name) values ("'+post.vote+'")';
+    queryExecute(sql,function (error, results, fields) {
+        if (error) throw error;
+        var returnStr = '';
+        send200(res,returnStr)
+    });
 
-// };
+};
 
 
 
-// postMethods.save_leader = function(res,post){
+postMethods.save_leader = function(res,post){
 
     
-//     //var returnStr = JSON.stringify(post);
+    //var returnStr = JSON.stringify(post);
 
-//     var returnStr = '<html><script>document.location.href="next.html";</script></html>';
-//     send200(res,returnStr);
+    var returnStr = '<html><script>document.location.href="next.html";</script></html>';
+    send200(res,returnStr);
 
-// };
+};
+
+postMethods.dice_num = function(res, post){
+    var sql="UPDATE rollingrole.groups SET leader_num='"+post.num+"' WHERE group_info='"+post.ip+"'";
+    queryExecute(sql,function (error, results, fields) {
+        if (error) throw error;
+        var returnStr = '';
+        send200(res,returnStr)
+    });
+}
+
+postMethods.role_name=function(res, post){
+    var sql="UPDATE roles SET role_name='"+post.role_name1+"', do_num="+post.do_num1+" WHERE (group_num=(SELECT group_num FROM groups WHERE group_info='"+post.ip+"') && role_num=1)";
+    queryExecute(sql,function (error, results, fields) {
+        if (error) throw error;
+        var returnStr = '';
+    }); 
+    sql="UPDATE roles SET role_name='"+post.role_name2+"', do_num="+post.do_num2+" WHERE (group_num=(SELECT group_num FROM groups WHERE group_info='"+post.ip+"') && role_num=2)";
+    queryExecute(sql,function (error, results, fields) {
+        if (error) throw error;
+        returnStr = '';
+    }); 
+    sql="UPDATE roles SET role_name='"+post.role_name3+"', do_num="+post.do_num3+" WHERE (group_num=(SELECT group_num FROM groups WHERE group_info='"+post.ip+"') && role_num=3)";
+    queryExecute(sql,function (error, results, fields) {
+        if (error) throw error;
+        returnStr = '';
+    }); 
+    sql="UPDATE roles SET role_name='"+post.role_name4+"', do_num="+post.do_num4+" WHERE (group_num=(SELECT group_num FROM groups WHERE group_info='"+post.ip+"') && role_num=4)";
+    queryExecute(sql,function (error, results, fields) {
+        if (error) throw error;
+        returnStr = '';
+    }); 
+    sql="UPDATE roles SET role_name='"+post.role_name5+"', do_num="+post.do_num5+" WHERE (group_num=(SELECT group_num FROM groups WHERE group_info='"+post.ip+"') && role_num=5)";
+    queryExecute(sql,function (error, results, fields) {
+        if (error) throw error;
+        returnStr = '';
+    }); 
+    sql="UPDATE roles SET role_name='"+post.role_name6+"', do_num="+post.do_num6+" WHERE (group_num=(SELECT group_num FROM groups WHERE group_info='"+post.ip+"') && role_num=6)";
+    queryExecute(sql,function (error, results, fields) {
+        if (error) throw error;
+        returnStr = '';
+    }); 
+    sql="UPDATE roles SET role_name='"+post.role_name7+"', do_num="+post.do_num7+" WHERE (group_num=(SELECT group_num FROM groups WHERE group_info='"+post.ip+"') && role_num=7)";
+    queryExecute(sql,function (error, results, fields) {
+        if (error) throw error;
+        returnStr = '';
+    }); 
+    sql="UPDATE roles SET role_name='"+post.role_name8+"', do_num="+post.do_num8+" WHERE (group_num=(SELECT group_num FROM groups WHERE group_info='"+post.ip+"') && role_num=8)";
+    queryExecute(sql,function (error, results, fields) {
+        if (error) throw error;
+        returnStr = '';
+    }); 
+    sql="UPDATE roles SET role_name='"+post.role_name9+"', do_num="+post.do_num9+" WHERE (group_num=(SELECT group_num FROM groups WHERE group_info='"+post.ip+"') && role_num=9)";
+    queryExecute(sql,function (error, results, fields) {
+        if (error) throw error;
+        returnStr = '';
+        send200(res,returnStr)  
+    });
+}       
